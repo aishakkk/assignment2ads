d7f374f753e37cc6e89e19a30faef46dcd12b923
Aisha <kalimatovaaisha@mail.ru>
2023-04-19 14:42:34 +0600

    adding method indexOf

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -94,7 +94,12 @@ public class MyArrayList<T> implements MyList{
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == o) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
