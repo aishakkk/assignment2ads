471dee058fa511f0397ed5ab70e60400a6018dc4
Aisha <kalimatovaaisha@mail.ru>
2023-04-19 14:42:55 +0600

    adding method lastIndexOf

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -104,7 +104,12 @@ public class MyArrayList<T> implements MyList{
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0;
+        for (int i = size - 1; i >= 0; i--) {
+            if (arr[i] == o) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
